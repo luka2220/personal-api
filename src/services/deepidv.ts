2f9674a8a2d49ed83934809319b16d5e75a1726c
Luka Piplica <piplicaluka64@gmail.com>
2026-01-08 22:14:48 -0500

    Add a check to see if the wh request signature was sent

--- src/services/deepidv.ts
+++ src/services/deepidv.ts
@@ -4,6 +4,8 @@ export const DeepidvService = new Hono();
 
 DeepidvService.post('/wh', async (c) => {
   const data = await c.req.json();
+  const signature = c.req.header('deepidv-signature');
   console.log('Data sent from deepidv: ', data);
+  console.log('Deepidv Signature: ', signature);
   return c.text('Pinged webhook endpoint');
 });
