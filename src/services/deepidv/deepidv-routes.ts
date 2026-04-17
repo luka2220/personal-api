3466f24ae6a74aa4773c01902de21e536b35617c
Luka Piplica <piplicaluka64@gmail.com>
2026-04-17 11:14:55 -0400

    test: remove deepidv webhook enpoint request validation

--- src/services/deepidv/deepidv-routes.ts
+++ src/services/deepidv/deepidv-routes.ts
@@ -4,16 +4,17 @@ import * as z from "zod";
 
 export const DeepidvService = new Hono();
 
-const TestWebhookSchema = z.object({
-  name: z.string(),
-  email: z.email(),
-  age: z.number().min(0).max(100),
-});
+// const TestWebhookSchema = z.object({
+//   name: z.string(),
+//   email: z.email(),
+//   age: z.number().min(0).max(100),
+// });
 
-DeepidvService.post("/wh", zValidator("json", TestWebhookSchema), (c) => {
-  const data = c.req.valid("json");
+DeepidvService.post("/wh", async (c) => {
+  // const data = c.req.valid("json");
+  const data = await c.req.json();
   const signature = c.req.header("deepidv-signature");
-  console.log("Data sent from deepidv: ", data);
+  console.log("Data sent from deepidv: ", JSON.stringify(data));
   console.log("Deepidv Signature: ", signature);
   return c.text("Pinged webhook endpoint");
 });
